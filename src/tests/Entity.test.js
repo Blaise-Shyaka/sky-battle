61b5cdd86e0a116e50599f4405bdb635e5301d99
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-04-03 20:39:24 +0200

    add sample tests

---
+++ src/tests/Entity.test.js
@@ -0,0 +1,7 @@
+import Entity from '../Entity';
+
+describe('The Main Scene', () => {
+  test('It should be an instance of a Function', () => {
+    expect(Entity).toBeInstanceOf(Function);
+  });
+});
---
+++ src/tests/MainScene.test.js
@@ -0,0 +1,7 @@
+import MainScene from '../MainScene';
+
+describe('The Main Scene', () => {
+  test('It should be an instance of a Function', () => {
+    expect(MainScene).toBeInstanceOf(Function);
+  });
+});
